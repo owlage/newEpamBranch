25924b3f6f01dfec481ebdbe3275217d99ffafcc
Viktoriya_Ponkratina <viktoriya_ponkratina@epam.com>
2020-12-08 20:36:40 +0300

    refactoring

--- src/text/Text.java
+++ src/text/Text.java
@@ -13,135 +13,18 @@ import java.util.regex.Pattern;
 
 public class Text {
 
-    public static void makeTextOperation(String text) {
-        System.out.println(text);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Выберите команду:\n" +
-                "1 - для сортировки абзацев по количеству предложений\n" +
-                "2 - для сортировки предложений по длинным слова\n" +
-                "3 - для сортировки лексема по возрастанию");
-        while (true) {
-            int choice = scanner.nextInt();
-            if(choice < 1 || choice > 3){
-                System.out.println("Выберите команду");
-                continue;
-            }
-            switch (choice) {
-                case 1:
-                    sortParagraphs(text);
-                    break;
-                case 2:
-                    sortWords(text);
-                    break;
-                case 3:
-                    sortLexemes(text);
-                    break;
-            }
-        }
+    private String value;
+
+    public String getValue() {
+        return value;
     }
 
-
-    private static void sortParagraphs(String text) {
-        String[] paragraphs = text.split("\n");
-        int[] counterSentences = new int[paragraphs.length];
-        int maxLengthParagraph = 0;
-        for (int i = 0; i < paragraphs.length; i++) {
-            String[] sentences = splitSentences(paragraphs[i]);
-            counterSentences[i] = sentences.length;
-            if (maxLengthParagraph < sentences.length) {
-                maxLengthParagraph = sentences.length;
-            }
-        }
-        for (int i = 1; i <= maxLengthParagraph; i++) {
-            for (int j = 0; j < counterSentences.length; j++) {
-                if (i == counterSentences[j]) {
-                    System.out.println(paragraphs[j]);
-                }
-            }
-        }
-        System.out.println();
+    public void setValue(String value) {
+        this.value = value;
     }
 
-    private static void sortWords(String text) {
-        String[] paragraphs = text.split("\n");
-        for (String paragraph : paragraphs) {
-            String[] sentences = splitSentences(paragraph);
-            for (String sentence : sentences) {
-                String[] words = splitWords(sentence);
-
-                for (int k = words.length - 1; k >= 0; k--) {
-                    for (int m = 0; m < k; m++) {
-                        if (words[m].length() > words[m + 1].length()) {
-                            String tmp = words[m];
-                            words[m] = words[m + 1];
-                            words[m + 1] = tmp;
-                        }
-                    }
-                }
-
-                for (String word : words) {
-                    System.out.print(word + " ");
-                }
-                System.out.print("\b. ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+    public Text(String value) {
+        this.value = value;
     }
 
-    private static void sortLexemes(String text){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите символ для сортировки лексем по размеру: ");
-        String letter = scanner.nextLine();
-        String[] paragraphs = text.split("\n");
-        for (String paragraph : paragraphs) {
-            String[] sentences = splitSentences(paragraph);
-            for (String sentence : sentences) {
-                String[] words = splitWords(sentence);
-
-                for (int k = words.length - 1; k >= 0; k--) {
-                    for (int m = 0; m < k; m++) {
-                        int countRight = 0;
-                        int countLeft = 0;
-                        for (int n = 0; n < words[m].length(); n++) {
-                            if (String.valueOf(words[m].charAt(n)).compareToIgnoreCase(letter) == 0) {
-                                countLeft++;
-                            }
-                        }
-                        for (int n = 0; n < words[m + 1].length(); n++) {
-                            if (String.valueOf(words[m + 1].charAt(n)).compareToIgnoreCase(letter) == 0) {
-                                countRight++;
-                            }
-                        }
-                        if (countLeft < countRight) {
-                            String tmp = words[m];
-                            words[m] = words[m + 1];
-                            words[m + 1] = tmp;
-                        } else if (countLeft == countRight) {
-                            String[] forCompare = {words[m], words[m + 1]};
-                            Arrays.sort(forCompare);
-                            words[m] = forCompare[0];
-                            words[m + 1] = forCompare[1];
-                        }
-                    }
-                }
-                for (String word : words) {
-                    System.out.print(word + " ");
-                }
-                System.out.print("\b. ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    private static String[] splitSentences(String text) {
-        Pattern pattern = Pattern.compile("\\.*[.!?]\\s*");
-        return pattern.split(text);
-    }
-
-    private static String[] splitWords(String sentence) {
-        Pattern pattern = Pattern.compile("\\s*(\\s|,|;|:)\\s*");
-        return pattern.split(sentence);
-    }
 }

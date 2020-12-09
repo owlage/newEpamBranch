3d200cfc871e13248474d86541183c82b9dbd69a
Sauchuk A <owlageone@gmail.com>
2020-12-09 23:40:43 +0300

    refactor

--- src/program/Program.java
+++ src/program/Program.java
@@ -8,7 +8,7 @@ public class Program {
     public static void main(String[] args) {
         TriangleService triangleService = new TriangleService();
         Triangle triangle = triangleService.createTriangle();
-        if(triangleService.checkTriangle(triangle) == 1){
+        if (triangleService.checkTriangle(triangle) == 1) {
             triangleService.showPerimeterAndSquareTriangle(triangle);
         }
     }

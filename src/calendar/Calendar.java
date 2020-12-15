7e31bfee9268db74d7dbd85249521abf1cbbfa2e
Sauchuk A <owlageone@gmail.com>
2020-12-15 13:40:53 +0300

    refactor

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Iterator;
 
 public class Calendar {
 
